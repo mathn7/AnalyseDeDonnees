1b001259cfb32b50f984354a2a376b6e1f5561b8
MohamedElWaghf <hindahwakhfe@gmail.com>
2020-06-18 18:24:42 +0200

    maj des dependances

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,7 @@
 using Documenter
-using Analyse_donnees
+
 
 makedocs(
-    modules = [Analyse_donnees],
     sitename = "Analyse_donnees.jl",
     authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
