b0d422d9d03de6adeabd93d233c878d67845217c
MohamedElWaghf <57914752+MohamedElWaghf@users.noreply.github.com>
2020-06-18 11:40:23 +0200

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter
+
+makedocs(
+    strict=true,
+    authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
+            "Accueil" => "index.md",            
+            "Sujet" => "Sujet.md"	        
+		
+            ]
+    )
+
+deploydocs(
+    repo   = "github.com/mathn7/optinum.git",
+    branch = "gh-pages",
+    devbranch = "master",
+)
