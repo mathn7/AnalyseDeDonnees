86677b5f5d963c9a630a3b35bccaa6fd7dd086c9
MohamedElWaghf <hindahwakhfe@gmail.com>
2020-06-19 13:14:23 +0200

    exercice 1 du Tp1

--- src/TP1/exercice_1.jl
+++ src/TP1/exercice_1.jl
@@ -1,93 +1,113 @@
-%--------------------------------------------------------------------------
-% ENSEEIHT - 1SN - Analyse de données
-% TP1 - Espace de représentation des couleurs
-% Exercice_1.m
-%--------------------------------------------------------------------------
+#=-------------------------------------------------------------------------
+ ENSEEIHT - 1SN - Analyse de donnees
+ TP1 - Espace de representation des couleurs
+  Exercice_1.js
+--------------------------------------------------------------------------
+=#
+using PyPlot
+using ImageView
+using TestImages, Gtk.ShortNames
+using Images
 
-clear
-close all
-clc
+ImageView.closeall()
 
-taille_ecran = get(0,'ScreenSize');
-L = taille_ecran(3);
-H = taille_ecran(4);
+#= "Decompostion des canaux RVB d'une image couleur "=#
 
-%% Décomposition des canaux RVB d'une image couleur
+I = testimage("mandrill")        #" 1er exemple"
 
-I = imread('autumn.tif');          % 1er exemple
-% I = imread('gantrycrane.png');    % 2nd exemple
-% I = imread('pears.png');          % 3ième exemple (dernier exercice)
-% I = imread('coloredChips.png');   % 4ième exemple (dernier exercice)
+#"Decoupage de l'image en trois canaux et conversion en flottants"
+C=channelview(I)
+B = colorview(RGB, zeroarray, zeroarray, C[3,:,:]);
+R = colorview(RGB, C[1,:,:], zeroarray, zeroarray);
+V = colorview(RGB, zeroarray, C[2,:,:], zeroarray);
 
-% Découpage de l'image en trois canaux et conversion en flottants
-R = single(I(:,:,1));
-V = single(I(:,:,2));
-B = single(I(:,:,3));
 
-%% Affichage de l'image RVB et de ses canaux
+##" Affichage de l'image RVB et de ses canaux"
 
-% 1ère fenêtre d'affichage
-figure('Name','Separation des canaux RVB',...
-       'Position',[0.01*L,0.1*H,0.59*L,0.75*H])
+#" Affichage de l'image RVB"
 
-% Affichage de l'image RVB
-    subplot(2,2,1)	% La fenêtre comporte 2 lignes et 2 colonnes
-    imagesc(I)
-    axis off image
-    title('Image RVB','FontSize',20)
-    
-colormap gray % Pour afficher les images en niveaux de gris
-              % Affichage avec la palette 'parula' par défaut sinon
-    
-% Affichage du canal R
-    subplot(2,2,2) % 1ère ligne, 2ème colonne
-    imagesc(R)
-    axis off image
-    title('Canal R','FontSize',20)
-    
-% Affichage du canal V
-    subplot(2,2,3) % 2ème ligne, 1ère colonne
-    imagesc(V)
-    axis off image
-    title('Canal V','FontSize',20)
-    
-% Affichage du canal B
-    subplot(2,2,4) % 2ème ligne, 2ème colonne
-    imagesc(B)
-    axis off image
-    title('Canal B','FontSize',20)
-    
-% Enregistrement des images des canaux
-imwrite(uint8(R),'canal_R.png')
-imwrite(uint8(V),'canal_V.png')
-imwrite(uint8(B),'canal_B.png')
+#figure("Affichage de l'image RVB et de ses canaux",figsize=(30,30));
 
-%% Affichage du nuage de pixels dans le repère RVB
+grid, frames, canvases = canvasgrid((2,2));
+ImageView.imshow(canvases[1,1], testimage("mandrill"))
 
-% % Deuxième fenêtre d'affichage
-% figure('Name','Nuage de pixels dans le repere RVB',...
-%        'Position',[0.61*L,0.1*H,0.38*L,0.6*H])
-% 
-%     plot3(R,V,B,'b.')
-%     axis equal
-%     grid on
-%     xlabel('R','FontWeight','bold')
-%     ylabel('V','FontWeight','bold')
-%     zlabel('B','FontWeight','bold')
-%     title({'Représentation 3D des pixels' ...
-%            'dans l''espace RVB'},'FontSize',20)
-%     rotate3d 
+#" Affichage du canal R"
 
-%% Calcul des corrélations entre les canaux RVB et des contrastes
-    
-% Matrice des données
-X = [R(:) V(:) B(:)];	% Les trois canaux sont vectorisés et concaténés
+#figure("Affichage du canal R",figsize=(30,30));
+#title("Affichage du canal R ")
 
-% Matrice de variance/covariance
-% A REMPLIR %
+ImageView.imshow(canvases[1,2],R)
 
-% Coefficients de corrélation linéaire
-% A REMPLIR %
+#" Affichage du canal V"
 
-% Proportions de contraste
-% A REMPLIR %
+#figure("Affichage du canal V",figsize=(30,30));
+#title("Affichage du canal V")
+
+ImageView.imshow(canvases[2,1],V)
+
+#" Affichage du canal B"
+
+#figure("Affichage du canal B",figsize=(30,30));
+#title("Affichage du canal B")
+
+ImageView.imshow(canvases[2,2],Gray.(B))
+win = Window(grid)
+showall(win)
+
+
+#" Enregistrement des images des canaux "
+#save("TP Analyse de Donnees/canal_R.png",img)
+#imwrite(uint8(V),'canal_V.png')
+#imwrite(uint8(B),'canal_B.png')
+
+#" Affichage du nuage de pixels dans le repere RVB "
+
+R_gray = Gray.(R)
+R_gray_float=convert(Array{Gray{Float64},2},R)
+R= Real.(R_gray_float).*100;
+B_gray = Gray.(B)
+B_gray_float=convert(Array{Gray{Float64},2},B)
+B= Real.(B_gray_float).*100;
+V_gray = Gray.(V)
+V_gray_float=convert(Array{Gray{Float64},2},V)
+V= Real.(V_gray_float).*100;
+
+#= " Deuxieme fenetre d affichage  " =#
+figure("Nuage de pixels dans le repere RVB ",figsize=(30,30))
+title("Representation 3D des pixels dans lespace RVB",FontSize=20)
+axis("equal")
+xlabel("R")
+ylabel("V")
+zlabel("B")
+PyPlot.surf(R,V,B)
+
+#" Calcul des correlations entre les canaux RVB et des contrastes"
+
+#" Matrice des donnees "
+
+X = [R[:] V[:] B[:]];	#"Les trois canaux sont vectorises et concatenes"
+
+#" Matrice de variance/covariance"
+
+n=length(X)
+X_bar= mean(X);
+Sigma = 1/n.*transpose(X)*X-X_bar*X_bar;
+
+# "Coefficients de correlation lineaire"
+sigR=(abs(Sigma[1,1]))^(1/2);
+sigV=(abs(Sigma[2,2]))^(1/2);
+sigB=(abs(Sigma[3,3]))^(1/2);
+correlationRB=Sigma[1,3];
+correlationVB=Sigma[2,3];
+correlationVR=Sigma[1,2];
+rRB=correlationRB/(sigR*sigB);
+rVB=correlationVB/(sigV*sigB);
+rVR=correlationVR/(sigV*sigR);
+#" Proportions de contraste"
+Variance_total=(sigR^2)+(sigB^2)+(sigV^2);
+cB=sigB^2/Variance_total;
+cV=sigV^2/Variance_total;
+cR=sigR^2/Variance_total;
+
+
+
