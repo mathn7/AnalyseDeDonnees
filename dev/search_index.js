1ae1a923f34fe65ffdd819772c52f2492f9830f1
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-19 09:58:38 +0000

    build based on 15e23e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"Sujet/#Sujet-1","page":"Sujet","title":"Sujet","text":"","category":"section"},{"location":"#Analyse_donnees-1","page":"Accueil","title":"Analyse_donnees","text":"","category":"section"},{"location":"#","page":"Accueil","title":"Accueil","text":"Le package Optinum constitue une  solution du projet d'Analyse numérique pour l’année 2020-2021 de l'École INP-ENSEEIHT,  et contient tous les algorithmes vus en cours.","category":"page"},{"location":"#Auteurs-:-1","page":"Accueil","title":"Auteurs :","text":"","category":"section"}]
+}
