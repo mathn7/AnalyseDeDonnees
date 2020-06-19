06536f9d9ceca9441b855c7798469a4cc021597a
MohamedElWaghf <57914752+MohamedElWaghf@users.noreply.github.com>
2020-06-19 13:19:13 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
 using Documenter
+using Analyse_donnees
 
 makedocs(
+    modules = [Analyse_donnees],
     sitename = "Analyse_donnees.jl",
     authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
