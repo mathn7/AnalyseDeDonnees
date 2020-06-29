00333a5103e25bbb3c0f851dc1089e8369bd075e
MohamedElWaghf <hindahwakhfe@gmail.com>
2020-06-29 13:54:22 +0200

    maj fonctions et tests tp1

--- src/TP1/exercice_1.jl
+++ src/TP1/exercice_1.jl
@@ -13,109 +13,118 @@ using TestImages, Gtk.ShortNames
 using Images
 using ImageMagick
 using Statistics
-using LinearAlgebra
 using Printf
+using LinearAlgebra
+
+#Entrée : matrice representatrice l'image
+#Sortie : la matrice des données
+function Matrice_de_donnees(Im)
+    #Decoupage de l'image en trois canaux et conversion en flottants
+    C = channelview(Im);
+    R = float(C[1,:,:]);
+    V = float(C[2,:,:]);
+    B = float(C[3,:,:]);
+    # Matrice des donnees
+    X = [R[:] V[:] B[:]];	#Les trois canaux sont vectorises et concatenes
+    return X
+end
+
+#Entrée : matrice representatrice l'image
+#Sortie : l'individu moyen
+function Individu_moyen(Im)
+    (n,p)= size(Matrice_de_donnees(Im));
+    return Matrice_de_donnees(Im)'*ones(n,1)/n;
+end
+
+#Entrée : matrice representatrice l'image
+#Sortie : matrice des données centrés
+function Donnees_centrees(Im)
+    (n,p)= size(Matrice_de_donnees(Im));
+    return Matrice_de_donnees(Im)-ones(n,1)*Individu_moyen(Im)';
+end
+
+#Entrée : matrice representatrice l'image
+#Sortie : la matrice Sigma, de variance/covariance des variables aléatoires
+function Matrice_var_cov(Im)
+    (n,p)= size(Matrice_de_donnees(Im));
+    return (Donnees_centrees(Im)')*Donnees_centrees(Im)/n;
+end
 
 #Entrée : matrice representatrice l'image
 
-#Sortie : la matrice Sigma, de variance/covariance des variables aléatoires
-#         correspondant aux trois canaux R, V et B (matrice de taille 3 × 3)
+function affichages(Im)
 
-function Matrice_var_cov(I)
+    "#Decoupage de l'image en trois canaux et conversion en flottants"
+    C = channelview(Im);
+    R = float(C[1,:,:]);
+    V = float(C[2,:,:]);
+    B = float(C[3,:,:]);
 
-#nettoyer l'environnement
-ImageView.closeall()
-clf() #à commenter si vous n'êtes pas sous Atom
+    #nettoyer l'environnement
+    ImageView.closeall()
+    clf() #à commenter si vous n'êtes pas sous Atom
 
+    gui = imshow_gui((300,300),(2, 2))  # La fenetre comporte 2 lignes et 2 colonnes (affichage 300×300)
+    canvases = gui["canvas"]
 
-#pygui(true) #pour afficher l'image en dehors d'atom
-"# Decompostion des canaux RVB d'une image couleur #"
+    "###### Affichage de l'image RVB et de ses canaux  #######"
 
-#I = testimage("mandrill")        #" exemple d'une image tiréé du package ImageMagick
+    "# Affichage de l'image RVB"
 
-#I = load("src/TP1/automn.tiff")        #" 1er exemple d'une image locale"
+    ImageView.imshow(canvases[1,1], Im) #1ere ligne ,1ere colonne
 
-"#Decoupage de l'image en trois canaux et conversion en flottants"
-C = channelview(I);
-R = float(C[1,:,:]);
-V = float(C[2,:,:]);
-B = float(C[3,:,:]);
+    "# Affichage du canal R"
 
+    ImageView.imshow(canvases[1,2],R) #1ere ligne ,2nd colonne
 
-gui = imshow_gui((300,300),(2, 2))  # La fenetre comporte 2 lignes et 2 colonnes (affichage 300×300)
-canvases = gui["canvas"]
+    "#Affichage du canal V"
 
-"###### Affichage de l'image RVB et de ses canaux  #######"
+    ImageView.imshow(canvases[2,1],V) #2nd ligne ,1ere colonne
 
-"# Affichage de l'image RVB"
+    "# Affichage du canal B"
 
-ImageView.imshow(canvases[1,1], I) #1ere ligne ,1ere colonne
+    ImageView.imshow(canvases[2,2],B) #2nd ligne ,2nd colonne
 
-"# Affichage du canal R"
+    "#affichage de tous les images"
 
-ImageView.imshow(canvases[1,2],R) #1ere ligne ,2nd colonne
-
-"#Affichage du canal V"
-
-ImageView.imshow(canvases[2,1],V) #2nd ligne ,1ere colonne
-
-"# Affichage du canal B"
-
-ImageView.imshow(canvases[2,2],B) #2nd ligne ,2nd colonne
-
-"#affichage de tous les images"
-
-Gtk.showall(gui["window"])
+    Gtk.showall(gui["window"])
 
 
-"# Enregistrement des images des canaux "
+    "# Enregistrement des images des canaux "
 
-save("src/TP1/canal_R.png",C[1,:,:])   #pour enregistrer l'image coloré
-                               #save("src/TP1/canal_R.png",colorview(RGB, C[1,:,:], zeroarray, zeroarray))
-save("src/TP1/canal_V.png",C[2,:,:])   #pour enregistrer l'image coloré
-                               #save("src/TP1/canal_V.png",colorview(RGB,  zeroarray,C[2,:,:], zeroarray))
-save("src/TP1/canal_B.png",C[3,:,:])   #pour enregistrer l'image coloré
-                               #save("src/TP1/canal_B.png",colorview(RGB, zeroarray, zeroarray, C[3,:,:]))
+    save("src/TP1/canal_R.png",C[1,:,:])   #pour enregistrer l'image coloré
+                                   #save("src/TP1/canal_R.png",colorview(RGB, C[1,:,:], zeroarray, zeroarray))
+    save("src/TP1/canal_V.png",C[2,:,:])   #pour enregistrer l'image coloré
+                                   #save("src/TP1/canal_V.png",colorview(RGB,  zeroarray,C[2,:,:], zeroarray))
+    save("src/TP1/canal_B.png",C[3,:,:])   #pour enregistrer l'image coloré
+                                   #save("src/TP1/canal_B.png",colorview(RGB, zeroarray, zeroarray, C[3,:,:]))
 
-"####### Calcul des correlations entre les canaux RVB et des contrastes  #####"
+   "# Coefficients de correlation lineaire"
+   Sigma = Matrice_var_cov(Im) #Matrice de variance/covariance
 
-"# Matrice des donnees "
+   @printf("Correlation r(R,V) = %0.3f\n",Sigma[1,2]/sqrt(Sigma[1,1]*Sigma[2,2]));
+   @printf("Correlation r(R,B) = %0.3f\n",Sigma[1,3]/sqrt(Sigma[1,1]*Sigma[3,3]));
+   @printf("Correlation r(V,B) = %0.3f\n",Sigma[2,3]/sqrt(Sigma[2,2]*Sigma[3,3]));
 
-X = [R[:] V[:] B[:]];	"#Les trois canaux sont vectorises et concatenes"
+   "# Proportions de contraste"
 
-"# Matrice de variance/covariance"
+   c = sum(diag(Sigma));
+   @printf("Proportion de contraste dans le canal R = %0.3f\n",Sigma[1,1]/c);
+   @printf("Proportion de contraste dans le canal V = %0.3f\n",Sigma[2,2]/c);
+   @printf("Proportion de contraste dans le canal B = %0.3f\n",Sigma[3,3]/c);
 
-(n,p)= size(X);
-x_barre = X'*ones(n,1)/n;
-X_c = X-ones(n,1)*x_barre';	# Centrage des donnees
-Sigma = (X_c')*X_c/n;
+    "#Affichage du nuage de pixels dans le repere RVB "
 
-"# Coefficients de correlation lineaire"
-#=
-@printf("Correlation r(R,V) = %0.3f\n",Sigma[1,2]/sqrt(Sigma[1,1]*Sigma[2,2]));
-@printf("Correlation r(R,B) = %0.3f\n",Sigma[1,3]/sqrt(Sigma[1,1]*Sigma[3,3]));
-@printf("Correlation r(V,B) = %0.3f\n",Sigma[2,3]/sqrt(Sigma[2,2]*Sigma[3,3]));
+    #Deuxieme fenetre d affichage
+    PyPlot.scatter3D(R,V,B,"b.")
+    PyPlot.title("Representation 3D des pixels dans lespace RVB",FontSize=20)
+    PyPlot.xlabel("R")
+    PyPlot.ylabel("V")
+    PyPlot.zlabel("B")
 
-"# Proportions de contraste"
+    "#pour que la derniére fenêtre s'affiche sous Atom il faut que cette commande
+     #soit la dernière commande !"
 
-c = sum(diag(Sigma));
-@printf("Proportion de contraste dans le canal R = %0.3f\n",Sigma[1,1]/c);
-@printf("Proportion de contraste dans le canal V = %0.3f\n",Sigma[2,2]/c);
-@printf("Proportion de contraste dans le canal B = %0.3f\n",Sigma[3,3]/c);
-=#
-"#Affichage du nuage de pixels dans le repere RVB "
+    gcf() #à commmenter si vous n'êtes pas sous Atom
 
-#Deuxieme fenetre d affichage
-PyPlot.scatter3D(R,V,B,"b.")
-PyPlot.title("Representation 3D des pixels dans lespace RVB",FontSize=20)
-PyPlot.xlabel("R")
-PyPlot.ylabel("V")
-PyPlot.zlabel("B")
-
-"#pour que la derniére fenêtre s'affiche sous Atom il faut que cette commande
- #soit la dernière commande !"
-
-gcf() #à commmenter si vous n'êtes pas sous Atom
-
-return Sigma
 end
--- test/tester_tp1.jl
+++ test/tester_tp1.jl
@@ -4,14 +4,18 @@ using Statistics
 include("../src/TP1/exercice_1.jl")
 
 #tester le scripts de l'exercice 1 du Tp1 avec l'image automn.tiff
-tolerance = 1e-2
+tolerance = 1e-3
 
 #charger l'image à tester
-I = load("src/TP1/automn.tiff")
-Sigma = Matrice_var_cov(I);
+Im = load("src/TP1/automn.tiff");
+
+#l'individu moyen pour l'image automn
+Ind_moyen = [0.469,0.432,0.388]
+#matrice de variance covariance
+Sigma = Matrice_var_cov(Im);
 
 #tester les coefficients de corrélation linéaire
-#et les proportions de contraste
+#et les proportions de contraste pour l'image automn
 r_R_V = 0.986
 r_R_B = 0.969
 r_V_B = 0.992
@@ -21,16 +25,21 @@ PcB = 0.318
 
 c = sum(diag(Sigma));
 
-@testset "tester les coefficients de corrélation et proportions de contraste" begin
-	@testset "coefficients de corrélation" begin
-		@test isapprox(r_R_V,Sigma[1,2]/sqrt(Sigma[1,1]*Sigma[2,2]),atol=tolerance)
-		@test isapprox(r_R_B,Sigma[1,3]/sqrt(Sigma[1,1]*Sigma[3,3]),atol=tolerance)
-		@test isapprox(r_V_B,Sigma[2,3]/sqrt(Sigma[2,2]*Sigma[3,3]),atol=tolerance)
+@testset "tester l'individu moyen et la matrice de variance/covariance" begin
+	@testset "test de l'individu moyen" begin
+		@test isapprox(Ind_moyen,Individu_moyen(Im),atol=tolerance)
 	end
-	@testset "proportions de contraste" begin
-		@test isapprox(PcR,Sigma[1,1]/c,atol=tolerance)
-		@test isapprox(PcV,Sigma[2,2]/c,atol=tolerance)
-		@test isapprox(PcB,Sigma[3,3]/c,atol=tolerance)
+	@testset "test de la matrice de variance/covariance" begin
+		@testset "coefficients de corrélation" begin
+			@test isapprox(r_R_V,Sigma[1,2]/sqrt(Sigma[1,1]*Sigma[2,2]),atol=tolerance)
+			@test isapprox(r_R_B,Sigma[1,3]/sqrt(Sigma[1,1]*Sigma[3,3]),atol=tolerance)
+			@test isapprox(r_V_B,Sigma[2,3]/sqrt(Sigma[2,2]*Sigma[3,3]),atol=tolerance)
+		end
+		@testset "proportions de contraste" begin
+			@test isapprox(PcR,Sigma[1,1]/c,atol=tolerance)
+			@test isapprox(PcV,Sigma[2,2]/c,atol=tolerance)
+			@test isapprox(PcB,Sigma[3,3]/c,atol=tolerance)
+		end
 	end
 
 end
