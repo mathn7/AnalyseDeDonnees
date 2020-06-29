6007e542d8f0d0b2b1b8aa3f605785653496e648
relmo <elmontassir.rachid1937@gmail.com>
2020-06-29 18:44:19 +0200

    s

--- src/TP4/kppv.jl
+++ src/TP4/kppv.jl
@@ -5,79 +5,79 @@
 #--------------------------------------------------------------------------
 function kppv(DataA,DataT,labelA,labelT,K,ListeClass)
 
-Na= size(DataA)[1];
-Nt =size(DataT)[1];
+    Na= size(DataA)[1];
+    Nt =size(DataT)[1];
 
-Nt_test =10; # A changer, pouvant aller jusqu a Nt
+    Nt_test =10; # A changer, pouvant aller jusqu a Nt
 
-#" Initialisation de la matrice de confusion pour comparer les resultats"
-#" obtenus avec l'etiquetage deja present dans 'labelT' pour les images tests"
-confusion = zeros(length(ListeClass));
+    #" Initialisation de la matrice de confusion pour comparer les resultats"
+    #" obtenus avec l'etiquetage deja present dans 'labelT' pour les images tests"
+    confusion = zeros(length(ListeClass));
 
-# "Initialisation du vecteur d etiquetage des images tests"
-Partition = zeros(Nt_test,1)
+    # "Initialisation du vecteur d etiquetage des images tests"
+    Partition = zeros(Nt_test,1)
 
-#"Initialisation du nombre d erreur de reconnaissance"
-nb_erreurs = 0;
+    #"Initialisation du nombre d erreur de reconnaissance"
+    nb_erreurs = 0;
 
-print("Classification des images test dans " ,string(length(ListeClass)),"  classes ")
-print(" par la methode des ", string(K), " plus proches voisins:")
+    print("Classification des images test dans " ,string(length(ListeClass)),"  classes ")
+    print(" par la methode des ", string(K), " plus proches voisins:")
 
-# Boucle sur les vecteurs test de l ensemble de levaluation
-for i = 1:Nt_test
+    # Boucle sur les vecteurs test de l ensemble de levaluation
+    for i = 1:Nt_test
 
-    print("image test n: ",string(i))
+        print("image test n: ",string(i))
 
-    # Calcul des distances entre les vecteurs de test
-    # et les vecteurs d apprentissage [voisins]
+        # Calcul des distances entre les vecteurs de test
+        # et les vecteurs d apprentissage [voisins]
 
-    distance = sum((DataA - repmat(transpose(DataT[i,:]),Na,1)).^2,2);
-    distance=distance[:];
-    # On ne garde que les indices des K + proches voisins
+        distance = sum((DataA - repmat(transpose(DataT[i,:]),Na,1)).^2,2);
+        distance=distance[:];
+        # On ne garde que les indices des K + proches voisins
 
-    ind_ppv=sortperm(distance)
-    ind_ppv=ind_ppv[1:K]
+        ind_ppv=sortperm(distance)
+        ind_ppv=ind_ppv[1:K]
 
-    # Comptage du nombre de voisins appartenant a chaque classe
+        # Comptage du nombre de voisins appartenant a chaque classe
 
-    classes_kppv =labelA[ind_ppv]
-    nech = zeros(K,1);
-    for j = 1:length(ListeClass)
-        for k =1:length(classes_kppv)
-            if classes_kppv[k] == ListeClass[j]
-                nech[j]=nech[j]+1;
+        classes_kppv =labelA[ind_ppv]
+        nech = zeros(K,1);
+        for j = 1:length(ListeClass)
+            for k =1:length(classes_kppv)
+                if classes_kppv[k] == ListeClass[j]
+                    nech[j]=nech[j]+1;
+                end
             end
         end
+        nech=nech[:];
+        # Recherche de la classe contenant le maximum de voisins
+
+        #ind_sort=sortperm(nech)
+        #ind_max_kppv=ind_sort[length(nech)]
+        ind_max_kppv=sortperm(nech)[length(nech)];
+
+        # "Si l image test a le plus grand nombre de voisins dans plusieurs"
+        # " classes differentes, alors on lui assigne celle du voisin le + proche"
+        # " sinon on lui assigne l unique classe contenant le plus de voisins"
+
+        if length(ind_max_kppv)>1
+            classe_test = labelA[ind_ppv[]];
+        else
+            classe_test = ListeClass[ind_max_kppv];
+        end
+
+        # "Assignation de letiquette correspondant à la classe trouvee au point"
+        # "correspondant a la i-eme image test dans le vecteur 'Partition'"
+        Partition[i] = classe_test;
+
+        # Mise a jour de la matrice de confusion
+        confusion[labelT[i]+1, classe_test+1] = confusion[labelT[i]+1, classe_test+1] + 1;
+
+        # Mise a jour du nombre d'erreur
+        if classe_test ~= labelT[i]
+            nb_erreurs = nb_erreurs + 1;
+        end
+
     end
-    nech=nech[:];
-    # Recherche de la classe contenant le maximum de voisins
-
-    #ind_sort=sortperm(nech)
-    #ind_max_kppv=ind_sort[length(nech)]
-    ind_max_kppv=sortperm(nech)[length(nech)];
-
-    # "Si l image test a le plus grand nombre de voisins dans plusieurs"
-    # " classes differentes, alors on lui assigne celle du voisin le + proche"
-    # " sinon on lui assigne l unique classe contenant le plus de voisins"
-
-    if length(ind_max_kppv)>1
-        classe_test = labelA[ind_ppv[]];
-    else
-        classe_test = ListeClass[ind_max_kppv];
-    end
-
-    # "Assignation de letiquette correspondant à la classe trouvee au point"
-    # "correspondant a la i-eme image test dans le vecteur 'Partition'"
-    Partition[i] = classe_test;
-
-    # Mise a jour de la matrice de confusion
-    confusion[labelT[i]+1, classe_test+1] = confusion[labelT[i]+1, classe_test+1] + 1;
-
-    # Mise a jour du nombre d'erreur
-    if classe_test ~= labelT[i]
-        nb_erreurs = nb_erreurs + 1;
-    end
-
-end
-return Partition,confusion, nb_erreurs
+    return Partition,confusion, nb_erreurs
 end
