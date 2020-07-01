2cc8725050c9b38e002f24b005d942c2cce772f8
relmo <elmontassir.rachid1937@gmail.com>
2020-07-01 19:05:44 +0200

    modifier sources projet

--- src/Projet/clusters.jl
+++ src/Projet/clusters.jl
@@ -1,16 +1,15 @@
-clear
-close all
-load exercice_1
-figure('Name',['Plan des deux premieres composantes principales'],'Position',[0,0,0.67*L,0.67*H])
+
+vars = MAT.matread("exercice_1.mat")
+figure("Name",["Plan des deux premieres composantes principales"],"Position",[0,0,0.67*L,0.67*H])
 
 # Composantes principales des donnees d'apprentissage
 C = X_c*W			
 
-# Affichage des deux premieres composantes principales des donnees d'apprentissage :
+# Affichage des deux premieres composantes principales des donnees d"apprentissage :
 hold on
 for i = 1:nb_individus
 	lignes = (i-1)*nb_postures+1:i*nb_postures
-	plot(C(lignes,1),C(lignes,2),['*' 0.5*(rand(1,3)+1)],'MarkerSize',10,'LineWidth',2)
+	plot(C(lignes,1),C(lignes,2),["*" 0.5*(rand(1,3)+1)],"MarkerSize",10,"LineWidth",2)
 end
 proportion = 0.5
 ecart_x = max(C(:,1))-min(C(:,1))
@@ -21,6 +20,6 @@ min_y = min(C(:,2))-proportion*ecart_y
 max_y = max(C(:,2))+proportion*ecart_y
 axis([min_x max_x min_y max_y])
 axis equal
-set(gca,'FontSize',20)
-xlabel('$C_1$','FontSize',30,'Interpreter','Latex')
-ylabel('$C_2$','FontSize',30,'Interpreter','Latex')
+set(gca,"FontSize",20)
+xlabel("$C_1$","FontSize",30,"Interpreter","Latex")
+ylabel("$C_2$","FontSize",30,"Interpreter","Latex")
--- src/Projet/exercice_3.jl
+++ src/Projet/exercice_3.jl
@@ -1,54 +1,53 @@
-clear;
-close all;
-load donnees;
-load exercice_1;
-figure('Name','Image tiree aleatoirement','Position',[0.2*L,0.2*H,0.6*L,0.5*H]);
 
-% Seuil de reconnaissance a regler convenablement
-s = 1.0e+00;
+donnees = MAT.matread("donnees.mat")
+vars = MAT.matread("exercice_2.mat")
 
-% Pourcentage d'information 
-per = 0.95;
+figure("Name","Image tiree aleatoirement","Position",[0.2*L,0.2*H,0.6*L,0.5*H])
 
-% Tirage aleatoire d'une image de test :
-individu = randi(37);
-posture = randi(6);
-chemin = './Images_Projet_2018';
-fichier = [chemin '/' num2str(individu+3) '-' num2str(posture) '.jpg']
-Im=importdata(fichier);
-I=rgb2gray(Im);
-I=im2double(I);
-image_test=I(:)';
+# Seuil de reconnaissance a regler convenablement
+s = 1.0e+00
+
+# Pourcentage d"information 
+per = 0.95
+
+# Tirage aleatoire d"une image de test :
+individu = randi(37)
+posture = randi(6)
+chemin = "./Images_Projet_2018"
+fichier = [chemin "/" string(individu+3) "-" string(posture) ".jpg"]
+Im=importdata(fichier)
+I=rgb2gray(Im)
+I=im2double(I)
+image_test=I(:)'
  
 
-% Affichage de l'image de test :
-colormap gray;
-imagesc(I);
-axis image;
-axis off;
+# Affichage de l'image de test :
+colormap gray
+imagesc(I)
 
-% Nombre N de composantes principales a prendre en compte 
-% [dans un second temps, N peut etre calcule pour atteindre le pourcentage
-% d'information avec N valeurs propres] :
-N = 8;
 
-% N premieres composantes principales des images d'apprentissage :
+# Nombre N de composantes principales a prendre en compte 
+# [dans un second temps, N peut etre calcule pour atteindre le pourcentage
+# d"information avec N valeurs propres] :
+N = 8
+
+# N premieres composantes principales des images d"apprentissage :
 ...
 
-% N premieres composantes principales de l'image de test :
+# N premieres composantes principales de l"image de test :
 ...
 
-% Determination de l'image d'apprentissage la plus proche (plus proche voisin) :
+# Determination de l'image d'apprentissage la plus proche (plus proche voisin) :
 ...
 
-% Affichage du resultat :
+# Affichage du resultat :
 if ...
 	individu_reconnu = ...
-	title({['Posture numero ' num2str(posture) ' de l''individu numero '
-        num2str(individu+3)];...
-		['Je reconnais l''individu numero ' num2str(individu_reconnu+3)]},'FontSize',20);
+	title({["Posture numero " string(posture) " de l'individu numero "
+        string(individu+3)]...
+		["Je reconnais l'individu numero "string(individu_reconnu+3)]},"FontSize",20)
 else
-	title({['Posture numero ' num2str(posture) ' de l''individu numero '
-        num2str(individu+3)];...
-		'Je ne reconnais pas cet individu !'},'FontSize',20);
+	title({["Posture numero " string(posture) " de l'individu numero "
+        string(individu+3)]...
+		"Je ne reconnais pas cet individu !"},"FontSize",20)
 end
