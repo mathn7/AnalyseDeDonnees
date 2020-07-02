8346977e387d2d7bf6914a2ae7ce62c0c0ec7e9a
relmo <elmontassir.rachid1937@gmail.com>
2020-07-02 16:53:46 +0200

    update CSAD

--- src/TP2/affichage_ellipse.jl
+++ src/TP2/affichage_ellipse.jl
@@ -4,11 +4,11 @@
 
 function affichage_ellipse(C,theta_0,a,b,theta_points_ellipse,motif)
 
-         xy_points_ellipse = [a*cos.(theta_points_ellipse)  b*sin.(theta_points_ellipse)]';
-         R = [cos(theta_0) -sin(theta_0) ; sin(theta_0) cos(theta_0)];
-         xy_points_ellipse = R*xy_points_ellipse+C*ones(1,length(theta_points_ellipse));
-         px=[xy_points_ellipse[1,:]; xy_points_ellipse[1,1]]
-         py=[xy_points_ellipse[2,:]; xy_points_ellipse[2,1]]
-         plot(px,py,motif,MarkerSize=30,LineWidth=2);
+    xy_points_ellipse = [a*cos.(theta_points_ellipse)  b*sin.(theta_points_ellipse)]';
+    R = [cos(theta_0) -sin(theta_0) ; sin(theta_0) cos(theta_0)];
+    xy_points_ellipse = R*xy_points_ellipse+C*ones(1,length(theta_points_ellipse));
+    px=[xy_points_ellipse[1,:]; xy_points_ellipse[1,1]]
+    py=[xy_points_ellipse[2,:]; xy_points_ellipse[2,1]]
+    plot(px,py,motif,MarkerSize=30,LineWidth=2);
 
   end
