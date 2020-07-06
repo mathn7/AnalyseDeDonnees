02a3f5c8f55bf6de85a0899c477f07a9c58fccc0
MohamedElWaghf <hindahwakhfe@gmail.com>
2020-07-06 13:17:34 +0200

    updaete test affichage tp1

---
+++ test/test_affichage_tp1.jl
@@ -0,0 +1,25 @@
+using Markdown
+using PyPlot, Plots, Gtk.ShortNames , ImageView , ImageMagick
+
+include("../src/TP1/exercice_1.jl");
+include("../src/TP1/exercice_2.jl");
+
+
+# fonction
+wait_for_key(text) = (print(stdout, text); read(stdin, 1); nothing)
+
+#chargement de l'image de test
+Im = load("src/TP1/automn.tiff");
+
+println("Lancement des tests d'affichage ...")
+
+wait_for_key("tapez entrer pour lancer l'exo 1")
+affichages(Im)
+println("\n")
+
+wait_for_key("tapez entrer pour lancer l'exo 2")
+affichages_2(Im)
+println("\n")
+
+wait_for_key("tapez entrer pour lancer l'exo 3")
+include("../src/TP1/exercice_3.jl")
