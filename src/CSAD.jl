7e9ca10dd50e907dbe4cc8098733e12886dec69b
relmo <rachid.elmontassir@etu.enseeiht.fr>
2020-07-06 13:50:08 +0200

    add CSAD.jl

--- src/CSAD.jl
+++ src/CSAD.jl
@@ -24,32 +24,15 @@ export Sigma2
 #================================#
 
 #============= TP3 ==============#
-include("TP3/couleur_moyenne.jl")
-export tp3_couleur_moyenne
-
-include("TP3/vraisemblance.jl")
-export tp3_vraisemblance
-
-include("TP3/moyenne_amelioree.jl")
-export tp3_moyenne_amelioree
-
-include("TP3/estimation_mu_sigma.jl")
-export tp3_estimation_mu_sigma
-
-include("TP3/exercice_1.jl")
-export tp3_exercice1
-
-include("TP3/exercice_2.jl")
-export tp3_exercice2
-
-include("TP3/exercice_3.jl")
-export tp3_exercice3
-
-include("TP3/exercice_3_bis.jl")
-export tp3_exercice3bis
-
-include("TP3/exercice_4.jl")
-export tp3_exercice4
+include("TP3/couleur_moyenne.jl");     export tp3_couleur_moyenne
+include("TP3/vraisemblance.jl");       export tp3_vraisemblance
+include("TP3/moyenne_amelioree.jl");   export tp3_moyenne_amelioree
+include("TP3/estimation_mu_sigma.jl"); export tp3_estimation_mu_sigma
+include("TP3/exercice_1.jl");          export tp3_exercice1
+include("TP3/exercice_2.jl");          export tp3_exercice2
+include("TP3/exercice_3.jl");          export tp3_exercice3
+include("TP3/exercice_3_bis.jl");      export tp3_exercice3bis
+include("TP3/exercice_4.jl");          export tp3_exercice4
 #================================#
 
 #============= TP4 ==============#
