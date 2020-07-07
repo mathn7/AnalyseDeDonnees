a72f0617faae9205e75a6873b2c17d17dcb32680
relmonta <46019567+relmonta@users.noreply.github.com>
2020-07-07 18:57:04 +0200

    Update kppv.jl

--- src/TP4/kppv.jl
+++ src/TP4/kppv.jl
@@ -20,7 +20,7 @@
    * **confusion**    : (Array{Float64,2})  la matrice de confusion
    * **nb_erreurs**   : (Int64) nombre d'erreurs effectuées lors de la classification
 """
-function kppv(afficher::Bool,DataA,DataT,labelA,labelT,K,ListeClass,Nt_test::Int64)
+function tp4_kppv(afficher::Bool,DataA,DataT,labelA,labelT,K,ListeClass,Nt_test::Int64)
 
     Na = size(DataA)[1]
     Nt = size(DataT)[1]
